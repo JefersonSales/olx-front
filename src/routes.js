9c46e428ce480dd4e81b9931fd0d5f5a5aaa1b87
Jeferson Sales <jefersonsales1@gmail.com>
2019-12-25 17:43:02 -0300

    Definindo rotas privadas e criação da página de adicionar um porst

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import RouteHandler from './components/route-handler';
 
 import Home from './pages/Home';
 import About from './pages/About';
@@ -7,28 +8,32 @@ import NotFound from './pages/NotFound';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import AdPage from './pages/AdPage';
+import AddAd from './pages/AddAd';
 
 export default () => {
   return (
     <Switch>
-      <Route exact path="/">
+      <RouteHandler exact path="/">
         <Home />
-      </Route>
-      <Route exact path="/about">
+      </RouteHandler>
+      <RouteHandler exact path="/about">
         <About />
-      </Route>
-      <Route exact path="/signin">
+      </RouteHandler>
+      <RouteHandler exact path="/signin">
         <SignIn />
-      </Route>
-      <Route exact path="/signup">
+      </RouteHandler>
+      <RouteHandler exact path="/signup">
         <SignUp />
-      </Route>
-      <Route exact path="/ad/:id">
+      </RouteHandler>
+      <RouteHandler exact path="/ad/:id">
         <AdPage />
-      </Route>
-      <Route>
+      </RouteHandler>
+      <RouteHandler exact path="/post-an-ad">
+        <Addad />
+      </RouteHandler>
+      <RouteHandler>
         <NotFound />
-      </Route>
+      </RouteHandler>
     </Switch>
   );
 };
